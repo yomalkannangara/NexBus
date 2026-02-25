96221ddb98445d9383a674975178df3ecd196f25
yomalkannangara <yomalkannax@gmail.com>
2026-02-25 09:19:31 +0530

    hosting pw user

--- config/database.php
+++ config/database.php
@@ -2,11 +2,11 @@
 use App\Support\Env;
 
 return [
-    'driver'   => Env::get('DB_CONNECTION', 'mysql'),
-    'host'     => Env::get('DB_HOST', '127.0.0.1'),
-    'port'     => Env::get('DB_PORT', '3306'),
+    'driver' => Env::get('DB_CONNECTION', 'mysql'),
+    'host' => Env::get('DB_HOST', '127.0.0.1'),
+    'port' => Env::get('DB_PORT', '3306'),
     'database' => Env::get('DB_DATABASE', 'nexbus'),
-    'username' => Env::get('DB_USERNAME', 'root'),
-    'password' => Env::get('DB_PASSWORD', ''),
-    'charset'  => Env::get('DB_CHARSET', 'utf8mb4'),
+    'username' => Env::get('DB_USERNAME', 'nexbususer'),
+    'password' => Env::get('DB_PASSWORD', 'Nexbus@12345'),
+    'charset' => Env::get('DB_CHARSET', 'utf8mb4'),
 ];
