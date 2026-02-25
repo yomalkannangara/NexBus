5bd585cec94396de72821a3270efc6c316c8c3c0
yomalkannangara <yomalkannax@gmail.com>
2026-02-25 10:18:06 +0530

    db

--- config/database.php
+++ config/database.php
@@ -6,7 +6,7 @@ return [
     'host' => Env::get('DB_HOST', '127.0.0.1'),
     'port' => Env::get('DB_PORT', '3306'),
     'database' => Env::get('DB_DATABASE', 'nexbus'),
-    'username' => Env::get('DB_USERNAME', 'nexbususer'),
-    'password' => Env::get('DB_PASSWORD', 'Nexbus@12345'),
+    'username' => Env::get('DB_USERNAME', 'root'),
+    'password' => Env::get('DB_PASSWORD', ''),
     'charset' => Env::get('DB_CHARSET', 'utf8mb4'),
 ];
